298ce7ef6fc1c85f3e3a43ada2b53037d4d49c00
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-17 21:49:52 +0800

    feat: get real data from db instead of fixed tasks

--- cmd/tasks/main_test.go
+++ cmd/tasks/main_test.go
@@ -1,7 +1,35 @@
 package main
 
-import "testing"
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
 
-func TestMain(t *testing.T) {
-	t.Skip("Skipping test for now")
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func TestMain(m *testing.M) {
+	setup()
+
+	code := m.Run()
+
+	teardown()
+	os.Exit(code)
+}
+
+func setup() {
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		log.Fatalf("Error opening test database: %v", err)
+	}
+	defer db.Close()
+}
+
+func teardown() {
+	// 删除测试用 SQLite3 数据库
+	err := os.Remove("test.db")
+	if err != nil {
+		log.Printf("Error removing test database: %v", err)
+	}
 }
