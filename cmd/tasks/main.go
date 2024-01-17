4336a14e3858ae001b4083e574c895617ceb8416
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-17 16:26:01 +0800

    feat: CrateTaskHandler should return result with Task struct

--- cmd/tasks/main.go
+++ cmd/tasks/main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/codegangsta/negroni"
 	"github.com/go-zoo/bone"
 	"net/http"
 )
 
+type Task struct {
+	Name   string `json:"name"`
+	Status int    `json:"status"`
+	ID     int    `json:"id"`
+}
+
 func createTaskHandler(w http.ResponseWriter, r *http.Request) {
+	newTask := Task{
+		Name:   "Eat Dinner",
+		Status: 0,
+		ID:     1,
+	}
+
+	response := map[string]interface{}{
+		"result": newTask,
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
 func main() {
