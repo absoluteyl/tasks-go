a37208c78d004630e6ccbd94b2136d9030f260d9
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-17 19:14:07 +0800

    feat: update task handler test with repository, service set

--- internal/handler/task_handler_test.go
+++ internal/handler/task_handler_test.go
@@ -2,12 +2,62 @@ package handler
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"github.com/absoluteyl/tasks-go/internal/model"
+	"github.com/absoluteyl/tasks-go/internal/repository"
+	"github.com/absoluteyl/tasks-go/internal/service"
+	_ "github.com/mattn/go-sqlite3"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
+
+var testDB *sql.DB
+var taskRepo *repository.TaskRepository
+var taskService *service.TaskService
+var taskHandler *TaskHandler
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
+
+func setup() {
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		fmt.Printf("Error opening test database: %v", err)
+		os.Exit(1)
+	}
+	testDB = db
+
+	createTableSQL := `
+	CREATE TABLE IF NOT EXISTS tasks (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT,
+		status INTEGER DEFAULT 0
+	);`
+	_, err = testDB.Exec(createTableSQL)
+	if err != nil {
+		fmt.Printf("Error creating tasks table: %v", err)
+		os.Exit(1)
+	}
+
+	taskRepo = repository.NewTaskRepository(testDB)
+	taskService = service.NewTaskServiceWithRepository(taskRepo)
+	taskHandler = NewTaskHandler(taskService)
+}
+
+func teardown() {
+	err := os.Remove("test.db")
+	if err != nil {
+		fmt.Printf("Error removing test database: %v", err)
+	}
+}
 
 func TestCreateTaskHandler(t *testing.T) {
 	taskData := map[string]interface{}{
@@ -25,7 +75,7 @@ func TestCreateTaskHandler(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	CreateTaskHandler(rr, req)
+	taskHandler.CreateTaskHandler(rr, req)
 
 	expectedHTTPStatus := http.StatusCreated
 	if status := rr.Code; status != expectedHTTPStatus {
