5b52bd978aa9777c1c58feac06726b33494a8ea0
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-17 16:46:56 +0800

    chore: move handler code into handler folder

--- cmd/tasks/main.go
+++ cmd/tasks/main.go
@@ -1,45 +1,15 @@
 package main
 
 import (
-	"encoding/json"
+	"github.com/absoluteyl/tasks-go/internal/handler"
 	"github.com/codegangsta/negroni"
 	"github.com/go-zoo/bone"
 	"net/http"
 )
 
-type Task struct {
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	Status int    `json:"status"`
-}
-
-func createTaskHandler(w http.ResponseWriter, r *http.Request) {
-	var newTask Task
-	err := json.NewDecoder(r.Body).Decode(&newTask)
-	if err != nil {
-		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
-		return
-	}
-
-	// FIXME: Temporary fixed status and id
-	newTask.ID = 1
-	newTask.Status = 0
-
-	response := map[string]interface{}{
-		"result": newTask,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-}
-
 func main() {
 	mux := bone.New()
-	mux.Post("/tasks/create", http.HandlerFunc(createTaskHandler))
+	mux.Post("/tasks/create", http.HandlerFunc(handler.CreateTaskHandler))
 
 	n := negroni.Classic()
 	n.UseHandler(mux)
---
+++ internal/handler/task_handler.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Task struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Status int    `json:"status"`
+}
+
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var newTask Task
+	err := json.NewDecoder(r.Body).Decode(&newTask)
+	if err != nil {
+		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+		return
+	}
+
+	// FIXME: Temporary fixed status and id
+	newTask.ID = 1
+	newTask.Status = 0
+
+	response := map[string]interface{}{
+		"result": newTask,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
