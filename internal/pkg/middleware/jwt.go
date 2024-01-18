54743f860a1f5afaf66b7297f5c3a7eb49726623
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-18 11:04:40 +0800

    feat: JWTMiddleware should return token invalid error when pass an invalid token

--- internal/pkg/middleware/jwt.go
+++ internal/pkg/middleware/jwt.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"net/http"
 	"strings"
 )
@@ -11,6 +12,21 @@ func JWTMiddleware(next http.Handler) http.Handler {
 
 		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
 			http.Error(w, "Authorization header is missing or not in 'Bearer {token}' format", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.NewValidationError("Unexpected signing method", jwt.ValidationErrorSignatureInvalid)
+			}
+
+			return []byte("secret"), nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid or expired token", http.StatusUnauthorized)
 			return
 		}
 
