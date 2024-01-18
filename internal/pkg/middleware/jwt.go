70ee3e03b418ee84ec18b8e3588c3a3178514491
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-18 11:06:02 +0800

    feat: JWTMiddleware should return error when token is older than 1 minute

--- internal/pkg/middleware/jwt.go
+++ internal/pkg/middleware/jwt.go
@@ -4,6 +4,7 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func JWTMiddleware(next http.Handler) http.Handler {
@@ -27,6 +28,22 @@ func JWTMiddleware(next http.Handler) http.Handler {
 
 		if err != nil || !token.Valid {
 			http.Error(w, "Invalid or expired token", http.StatusUnauthorized)
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			http.Error(w, "Invalid token claims", http.StatusUnauthorized)
+			return
+		}
+
+		if iat, ok := claims["iat"].(float64); ok {
+			if time.Now().After(time.Unix(int64(iat), 0).Add(1 * time.Minute)) {
+				http.Error(w, "Token is older than 1 minute", http.StatusUnauthorized)
+				return
+			}
+		} else {
+			http.Error(w, "Invalid issued at claim", http.StatusUnauthorized)
 			return
 		}
 
