70c4735fbc432ad6cb7b406bce5aabb2240d5a5b
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-18 11:03:13 +0800

    test: JWTMiddleware should return missing header error when header is not bearer token

--- internal/pkg/middleware/jwt_test.go
+++ internal/pkg/middleware/jwt_test.go
@@ -29,3 +29,29 @@ func TestJWTMiddleware_NoHeader(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
+
+func TestJWTMiddleware_HeaderInvalid(t *testing.T) {
+	req, err := http.NewRequest("GET", "/tasks", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Add("Authorization", "InvalidFormat")
+
+	rr := httptest.NewRecorder()
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	jwtHandler := JWTMiddleware(nextHandler)
+
+	jwtHandler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusUnauthorized)
+	}
+
+	expected := "Authorization header is missing or not in 'Bearer {token}' format\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
