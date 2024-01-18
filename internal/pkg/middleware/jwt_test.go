d50204c02823aa5f4b09d0b028351b31cce461ef
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-18 11:05:54 +0800

    test: JWTMiddleware should return error when token is older than 1 minute

--- internal/pkg/middleware/jwt_test.go
+++ internal/pkg/middleware/jwt_test.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestJWTMiddleware_NoHeader(t *testing.T) {
@@ -76,6 +78,39 @@ func TestJWTMiddleware_TokenInvalid(t *testing.T) {
 	}
 
 	expected := "Invalid or expired token\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestJWTMiddleware_TokenOlderThan1Minute(t *testing.T) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["iat"] = time.Now().Add(-2 * time.Minute).Unix()
+	tokenString, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("GET", "/tasks", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("Authorization", "Bearer "+tokenString)
+
+	rr := httptest.NewRecorder()
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	jwtHandler := JWTMiddleware(nextHandler)
+
+	jwtHandler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusUnauthorized)
+	}
+
+	expected := "Token is older than 1 minute\n"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
