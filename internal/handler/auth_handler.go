d7971d526c7a70fd92501afa65e78a09c92e530e
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-18 13:22:42 +0800

    feat: CreateAuthHandler should return token string

---
+++ internal/handler/auth_handler.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/absoluteyl/tasks-go/pkg/auth"
+	"net/http"
+)
+
+func CreateAuthHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: Authenticate if request is allow to gen token
+
+	token, err := auth.GenerateToken()
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]interface{}{
+		"token": token,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
