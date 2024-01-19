a4b730bc5ad8ff639ab967669b19ed818bbdc649
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-19 10:02:58 +0800

    refactor: auth handler test

--- internal/handler/auth_handler_test.go
+++ internal/handler/auth_handler_test.go
@@ -2,33 +2,44 @@ package handler
 
 import (
 	"encoding/json"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/absoluteyl/tasks-go/pkg/testutils"
 )
 
 func TestCreateAuthHandler(t *testing.T) {
-	req, err := http.NewRequest("POST", "/auth", nil)
-	if err != nil {
-		t.Fatalf("Error creating request: %v", err)
-	}
+	req := prepareCreateAuthRequest(t)
 
 	rr := httptest.NewRecorder()
 	CreateAuthHandler(rr, req)
 
-	expectedHTTPStatus := http.StatusOK
-	assert.Equal(t, expectedHTTPStatus, rr.Code, "Handler returned wrong status code")
+	testutils.HttpStatusShouldBe(t, rr, http.StatusOK)
 
+	response := parseMapResponse(t, rr)
+
+	token, ok := response["token"]
+	assert.True(t, ok, "Token field not found in response")
+
+	_, ok = token.(string)
+	assert.True(t, ok, "Token field is not a string")
+}
+func parseMapResponse(t *testing.T, rr *httptest.ResponseRecorder) map[string]interface{} {
 	var response map[string]interface{}
-	err = json.Unmarshal(rr.Body.Bytes(), &response)
+	err := json.Unmarshal(rr.Body.Bytes(), &response)
 	if err != nil {
 		t.Fatalf("Error unmarshaling JSON response: %v", err)
 	}
+	return response
+}
 
-	token, ok := response["token"]
-	assert.Truef(t, ok, "Token field not found in response")
-
-	_, ok = token.(string)
-	assert.Truef(t, ok, "Token field is not a string")
+func prepareCreateAuthRequest(t *testing.T) *http.Request {
+	req, err := http.NewRequest("POST", "/auth", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return req
 }
