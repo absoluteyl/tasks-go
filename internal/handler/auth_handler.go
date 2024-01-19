ea20beef9c182d482de0a4c9558a54fc99966f04
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2024-01-19 18:08:45 +0800

    feat: introduce basic auth for create auth api

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -3,12 +3,24 @@ package handler
 import (
 	"github.com/absoluteyl/tasks-go/pkg/auth"
 	"net/http"
+	"os"
+)
+
+var (
+	// FIXME: Get user credentials somewhere else
+	username = os.Getenv("USERNAME")
+	password = os.Getenv("PASSWORD")
 )
 
 func CreateAuthHandler(w http.ResponseWriter, r *http.Request) {
 	setContentType(w)
 
-	// TODO: Authenticate if request is allow to gen token
+	user, pass, ok := r.BasicAuth()
+	if !ok || user != username || pass != password {
+		w.Header().Set("WWW-Authenticate", `Basic realm="Please enter your username and password"`)
+		setErrResponse(w, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
 
 	token, err := auth.GenerateToken()
 	if err != nil {
--- internal/handler/auth_handler_test.go
+++ internal/handler/auth_handler_test.go
@@ -10,8 +10,25 @@ import (
 	. "github.com/absoluteyl/tasks-go/pkg/testutils"
 )
 
-func TestCreateAuthHandler(t *testing.T) {
-	req := prepareCreateAuthRequest(t)
+func TestAuthHandler(t *testing.T) {
+	t.Run("Create", testCreateAuthWithInvalidCredentials)
+	t.Run("Create", testCreateAuth)
+}
+
+func testCreateAuthWithInvalidCredentials(t *testing.T) {
+	req := prepareCreateAuthRequest(t, "invalid", "invalid")
+
+	rr := httptest.NewRecorder()
+	CreateAuthHandler(rr, req)
+
+	HttpStatusShouldBe(t, rr, http.StatusUnauthorized)
+
+	response := ParseResponse(t, rr)
+	ResultShouldBe(t, "Unauthorized", response["result"])
+}
+
+func testCreateAuth(t *testing.T) {
+	req := prepareCreateAuthRequest(t, username, password)
 
 	rr := httptest.NewRecorder()
 	CreateAuthHandler(rr, req)
@@ -25,10 +42,11 @@ func TestCreateAuthHandler(t *testing.T) {
 	assert.True(t, ok, "Result field is not a string")
 }
 
-func prepareCreateAuthRequest(t *testing.T) *http.Request {
+func prepareCreateAuthRequest(t *testing.T, user string, pass string) *http.Request {
 	req, err := http.NewRequest("POST", "/auth", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
+	req.SetBasicAuth(user, pass)
 	return req
 }
